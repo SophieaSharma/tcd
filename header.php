b378e5bc6c7f6ae53f65c1319f8e0e963d552e6c
sophieasharma111 <sophieasharma111@gmail.com>
2022-01-10 20:29:56 +0530

    changes in header file

--- header.php
+++ header.php
@@ -2,7 +2,9 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12 bg-light p-4 text-center h1">
-            TCD CAKE AND BAKE
+            <a href="signup_loginPage.php" style="text-decoration: none; color: black;">
+                TCD CAKE AND BAKE
+            </a>
         </div>
     </div>
 </div>
