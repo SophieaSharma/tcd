e911fd87f7660beeea7a02d5d9985e33f805713b
sophieasharma111 <sophieasharma111@gmail.com>
2022-01-31 11:17:25 +0530

    welcome modal

--- rawmaterial.php
+++ rawmaterial.php
@@ -9,6 +9,10 @@ GLOBAL $today,$total_price_of_all_items;
 if(!isset($_SESSION['id'])){
     header("location:login.php");
 }
+if(isset($_SESSION['id'])){?>
+
+
+<?php }
 ?>
 <!doctype html>
 <html lang="en">
@@ -18,18 +22,44 @@ if(!isset($_SESSION['id'])){
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css" type="text/css">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <title>ePortal</title>
+
 </head>
 <body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <!--heading-->
 <?php
 require_once "headerTCD.php";
 ?>
 <!--heading-->
+
+<!--welcome modal-->
+<div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered ">
+        <div class="modal-content" >
+                <div class="row m-3 border">
+                    <div class="col-12 mt-3 text-center">
+                        WELCOME <?php echo "<span class='text-uppercase'>{$_SESSION['name']}</span>"; ?>
+                    </div>
+                    <div class="col-12 my-3 text-center">
+                        <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Let's Go</button>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+</div>
+<script>
+    var myModal = new bootstrap.Modal(document.getElementById('myModal'), {})
+    myModal.show()
+</script>
+<!--welcome modal-->
 
 <!--main content-->
 <div class="container-fluid p-0">
